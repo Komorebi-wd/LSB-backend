6fdffb7ae1d6f58531189caadcc1f5ec8a99ffb5
wende <2504386283@qq.com>
2024-06-25 22:02:23 +0800

    init

---
+++ src/main/java/org/example/lsbbackend/LsbBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.lsbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsbBackendApplication.class, args);
+    }
+
+}
