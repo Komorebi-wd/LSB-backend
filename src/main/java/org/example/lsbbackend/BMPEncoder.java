67d50ca27d12e4561e7255606445be8c019d729e
栯辰 <12151311+yjzj0610@user.noreply.gitee.com>
2024-06-30 09:11:13 +0800

    11111

--- src/main/java/org/example/lsbbackend/BMPEncoder.java
+++ src/main/java/org/example/lsbbackend/BMPEncoder.java
@@ -21,7 +21,101 @@ public class BMPEncoder {
         }
 
     }
+    public static byte[] hideMessageInBmpWithoutKey(InputStream inputStream, String message) throws Exception{
+        try {
 
+            // 读取BMP文件
+            byte[] header = new byte[54];
+            inputStream.read(header);
+            // 读取图像宽度和高度
+            int width = ByteBuffer.wrap(header, 18, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            int height = ByteBuffer.wrap(header, 22, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+            // 读取像素数据
+            byte[] pixelData = new byte[inputStream.available()];
+            inputStream.read(pixelData);
+            inputStream.close();
+            // 将消息转换为二进制字符串
+            StringBuilder binaryMessage = new StringBuilder();
+            System.out.println(message);
+            System.out.println(message.getBytes(StandardCharsets.UTF_8).length);
+            System.out.println();
+            for (char i : message.toCharArray()) {
+                System.out.println(i);
+                System.out.println(Integer.toBinaryString(i));
+                String binaryChar = String.format("%16s", Integer.toBinaryString(i)).replace(' ', '0');
+                System.out.println(binaryChar);
+                binaryMessage.append(binaryChar);
+            }
+            System.out.println("MessageBinary:"+binaryMessage.length());
+            int size=binaryMessage.length();
+            // 补齐到3的倍数
+            while (binaryMessage.length() % 3 != 0) {
+                binaryMessage.append('0'); // 在末尾添加0，直到长度为3的倍数
+            }
+
+            System.out.println("MessageBinary:"+size);
+            String binary = Integer.toBinaryString(size & 0xFFFF);
+            // 如果字符串长度不足16位，前面补0直到长度为16位
+            binary = String.format("%16s", binary).replace(' ', '0');
+            binaryMessage.insert(0,binary);
+            System.out.println("message"+binaryMessage.toString());
+            // 检查消息是否可以嵌入到图像中
+            System.out.println();
+            int maxCapacity = pixelData.length * 8 / 3; // 每个像素可隐藏一个字符，每个像素占用3字节，一个字节8位
+            if (binaryMessage.length() > maxCapacity) {
+                throw new IllegalArgumentException("Message too long to embed in the image.");
+            }
+            for(int i=0;i<18;i+=3){
+                int b = pixelData[i] & 0xFF;
+                int g = pixelData[i + 1] & 0xFF;
+                int r = pixelData[i + 2] & 0xFF;
+                // 修改最低有效位为消息的一个bit
+                b = (b & 0xFE) | Character.getNumericValue(binaryMessage.charAt(i));
+                if(i==15){
+                    g = 0;
+                    r = 0;
+                }
+                else {
+                    g = (g & 0xFE) | Character.getNumericValue(binaryMessage.charAt(i + 1));
+                    r = (r & 0xFE) | Character.getNumericValue(binaryMessage.charAt(i + 2));
+                }
+                // 更新像素数据
+                pixelData[i] = (byte) b;
+                pixelData[i + 1] = (byte) g;
+                pixelData[i + 2] = (byte) r;
+
+            }
+            // 嵌入消息
+            int index = 16;
+            for (int byteIndex = 18; byteIndex < pixelData.length; byteIndex += 3) {
+                if (index < binaryMessage.length()) {
+                    // 读取原始像素颜色值
+                    int b = pixelData[byteIndex] & 0xFF;
+                    int g = pixelData[byteIndex+1] & 0xFF;
+                    int r = pixelData[byteIndex+2] & 0xFF;
+                    // 修改最低有效位为消息的一个bit
+                    b = (b & 0xFE) | Character.getNumericValue(binaryMessage.charAt(index));
+                    g = (g & 0xFE) | Character.getNumericValue(binaryMessage.charAt(index + 1));
+                    r = (r & 0xFE) | Character.getNumericValue(binaryMessage.charAt(index + 2));
+                    // 更新像素数据
+                    pixelData[byteIndex] = (byte) b;
+                    pixelData[byteIndex+1] = (byte) g;
+                    pixelData[byteIndex+2] = (byte) r;
+                    index += 3;
+                }
+                else{
+                    break;
+                }
+            }
+            byte[] combinedArray = new byte[header.length + pixelData.length];
+            System.arraycopy(header, 0, combinedArray, 0, header.length);
+            System.arraycopy(pixelData, 0, combinedArray, header.length, pixelData.length);
+            return combinedArray;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     public static byte[] hideMessageInBmp(InputStream inputStream, String message, String key) throws Exception{
         try {
 
@@ -86,14 +180,12 @@ public class BMPEncoder {
                     g = (g & 0xFE) | Character.getNumericValue(binaryMessage.charAt(i + 1));
                     r = (r & 0xFE) | Character.getNumericValue(binaryMessage.charAt(i + 2));
                 }
-
                 // 更新像素数据
                 pixelData[i] = (byte) b;
                 pixelData[i + 1] = (byte) g;
                 pixelData[i + 2] = (byte) r;
 
             }
-            System.out.println(binaryMessage.length());
             // 嵌入消息
             int index = 16;
             for (int byteIndex = 0; byteIndex < locations.length; byteIndex += 3) {
@@ -123,11 +215,6 @@ public class BMPEncoder {
             byte[] combinedArray = new byte[header.length + pixelData.length];
             System.arraycopy(header, 0, combinedArray, 0, header.length);
             System.arraycopy(pixelData, 0, combinedArray, header.length, pixelData.length);
-            // 写入修改后的像素数据到新的BMP文件
-//            FileOutputStream outputStream = new FileOutputStream(outputFilename);
-//            outputStream.write(header);
-//            outputStream.write(pixelData);
-//            outputStream.close();
             return combinedArray;
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/org/example/lsbbackend/controller/LSBController.java
+++ src/main/java/org/example/lsbbackend/controller/LSBController.java
@@ -23,6 +23,19 @@ import javax.imageio.ImageIO;
 @RequestMapping("/api/lsb")
 public class LSBController {
 
+    @PostMapping("/uploadWithKey")
+    public ResponseEntity<?> uploadImageWithKey(@RequestParam("image") MultipartFile file) {
+        try {
+            BufferedImage image = ImageIO.read(file.getInputStream());
+            if (image == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid image file.");
+            }
+            int maxEmbeddableLength = calculateMaxEmbeddableLength(file,true);
+            return ResponseEntity.ok(maxEmbeddableLength);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing image.");
+        }
+    }
     @PostMapping("/upload")
     public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) {
         try {
@@ -30,18 +43,17 @@ public class LSBController {
             if (image == null) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid image file.");
             }
-            int maxEmbeddableLength = calculateMaxEmbeddableLength(file);
+            int maxEmbeddableLength = calculateMaxEmbeddableLength(file,false);
             return ResponseEntity.ok(maxEmbeddableLength);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing image.");
         }
     }
-
-    @PostMapping("/embed")
-    public ResponseEntity<?> embedMessage(@RequestParam("image") MultipartFile imageFile,
-                                          @RequestParam("message") String message,
-                                          @RequestParam("key") String key
-                                          ) {
+    @PostMapping("/embedWithKey")
+    public ResponseEntity<?> embedMessageWithKey(@RequestParam("image") MultipartFile imageFile,
+                                                 @RequestParam("message") String message,
+                                                 @RequestParam("key") String key
+    ) {
         try {
 
 //            BufferedImage image = ImageIO.read(imageFile.getInputStream());
@@ -56,9 +68,26 @@ public class LSBController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error embedding message.");
         }
     }
+    @PostMapping("/embed")
+    public ResponseEntity<?> embedMessage(@RequestParam("image") MultipartFile imageFile,
+                                          @RequestParam("message") String message
+    ) {
+        try {
 
-    @PostMapping("/extract")
-    public ResponseEntity<?> extractMessage(@RequestParam("image") MultipartFile imageFile, @RequestParam("key") String key) {
+//            BufferedImage image = ImageIO.read(imageFile.getInputStream());
+//            BufferedImage modifiedImage = embedMessageIntoImage(image, message);
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            ImageIO.write(modifiedImage, "bmp", outputStream);
+//            byte[] imageBytes = outputStream.toByteArray();
+            byte[] imageBytes = BMPEncoder.hideMessageInBmpWithoutKey(imageFile.getInputStream(),message);
+            String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
+            return ResponseEntity.ok(encodedImage);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error embedding message.");
+        }
+    }
+    @PostMapping("/extractWithKey")
+    public ResponseEntity<?> extractMessageWithKey(@RequestParam("image") MultipartFile imageFile, @RequestParam("key") String key) {
         try {
             System.out.println(key);
             String message = BMPDecoder.extractMessageFromBmp(imageFile.getInputStream(),key);
@@ -68,26 +97,41 @@ public class LSBController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error extracting message.");
         }
     }
-
-    private int calculateMaxEmbeddableLength(MultipartFile image) throws Exception {
+    @PostMapping("/extract")
+    public ResponseEntity<?> extractMessage(@RequestParam("image") MultipartFile imageFile) {
+        try {
+            String message = BMPDecoder.extractMessageFromBmpWithoutKey(imageFile.getInputStream());
+            return ResponseEntity.ok(message);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error extracting message.");
+        }
+    }
+    private int calculateMaxEmbeddableLength(MultipartFile image,boolean isKey) throws Exception {
         InputStream inputStream=image.getInputStream();
         byte[] header = new byte[54];
         inputStream.read(header);
         int width = ByteBuffer.wrap(header, 18, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
         int height = ByteBuffer.wrap(header, 22, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
         int maxLength = 0;
-        if (header[28] == 24){
-            maxLength = (int)Math.floor((width * height*3 -16) / 256)*16;
+        if(isKey){
+            if (header[28] == 24){
+                maxLength = (int)Math.floor((width * height*3 -16) / 256)*16;
+            }
+            else if (header[28] == 8){
+                maxLength = (int)Math.floor((width * height -16) / 256)*16;
+            }
         }
-        else if (header[28] == 8){
-            maxLength = (int)Math.floor((width * height -16) / 256)*16;
+        else{
+            if (header[28] == 24){
+                maxLength = (width * height*3)/8;
+            }
+            else if (header[28] == 8){
+                maxLength = (width * height)/8;
+            }
         }
         System.out.println(width+"  "+height+"  "+maxLength);
         return maxLength;
-//        int width = image.getWidth();
-//        int height = image.getHeight();
-//        int maxLength = (width * height * 3-18) / 16-1; // 3 bytes per pixel, 1 bit per byte for LSB
-//        return maxLength;
     }
 
     private BufferedImage embedMessageIntoImage(BufferedImage image, String message) {
